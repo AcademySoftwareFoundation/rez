9dededb73872d9c0a31e33cdfe4840868fd03f9b
Chad Dombrova <chadrik@gmail.com>
2013-06-13 09:52:49 -0700

    rez_release: use copytree with hard links instead of a symbolic link to the directory

--- python/rez/rez_release.py
+++ python/rez/rez_release.py
@@ -168,7 +168,16 @@ class RezRelease(object):
 		This is particularly useful for revision control systems, which can
 		export a clean unmodified copy
 		'''
-		os.symlink(os.getcwd(), build_dir)
+		def ignore(src, names):
+			'''
+			returns a list of names to ignore, given the current list
+			'''
+			if src == self.base_dir:
+				return names
+			return [x for x in names if x.startswith('.')]
+
+		copytree(os.getcwd(), build_dir, symlinks=True,
+				ignore=ignore)
 
 	def pre_build(self):
 		'''
@@ -376,6 +385,50 @@ register_release_mode('base', RezRelease)
 ##############################################################################
 # Utilities
 ##############################################################################
+
+def copytree(src, dst, symlinks=False, ignore=None):
+	'''
+	copytree that supports hard-linking
+	'''
+	print "copying directory", src
+	names = os.listdir(src)
+	if ignore is not None:
+		ignored_names = ignore(src, names)
+	else:
+		ignored_names = set()
+
+	os.makedirs(dst)
+	errors = []
+	for name in names:
+		if name in ignored_names:
+			continue
+		srcname = os.path.join(src, name)
+		dstname = os.path.join(dst, name)
+		try:
+			if symlinks and os.path.islink(srcname):
+				linkto = os.readlink(srcname)
+				os.symlink(linkto, dstname)
+			elif os.path.isdir(srcname):
+				copytree(srcname, dstname, symlinks, ignore)
+			else:
+				#shutil.copy2(srcname, dstname)
+				os.link(srcname, dstname)
+		# XXX What about devices, sockets etc.?
+		except (IOError, os.error) as why:
+			errors.append((srcname, dstname, str(why)))
+		# catch the Error from the recursive copytree so that we can
+		# continue with other files
+		except shutil.Error as err:
+			errors.extend(err.args[0])
+	try:
+		shutil.copystat(src, dst)
+	except shutil.WindowsError:
+		# can't copy file access times on Windows
+		pass
+	except OSError as why:
+		errors.extend((src, dst, str(why)))
+	if errors:
+		raise shutil.Error(errors)
 
 class SvnValueCallback:
 	"""
