a8aa00d1418b6ddac6ada88b6f69fe9fb5f5aff9
Chad Dombrova <chadrik@gmail.com>
2013-09-16 18:37:16 -0700

    bugfix for rez_config: separate dot graph

--- python/rez/rez_config.py
+++ python/rez/rez_config.py
@@ -1034,7 +1034,7 @@ class _Configuration(object):
 		# test to see what adding this package would do
 		result, pkg = self.test_pkg_req_add(pkg_req, True)
 
-		self._add_package_to_dot_graph(pkg_req.short_name(), result, pkg,
+		self._add_package_to_dot_graph(pkg_req.short_name(), pkg, result,
 									   parent_pkg, dot_connection_type)
 
 		if (result == _Configuration.ADDPKG_CONFLICT):
