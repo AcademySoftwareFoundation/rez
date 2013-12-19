9f8963e6d1d9866c17398b7f5a2675abc840fbfa
Mark Streatfield <markst@al.com.au>
2013-12-19 12:18:26 +1100

    + Add future import of with statement for Python 2.5 compatibility. + Change Exception syntax to be compatible with Python 2.5.

--- python/rez/util.py
+++ python/rez/util.py
@@ -1,6 +1,9 @@
 """
 Misc useful stuff.
 """
+
+from __future__ import with_statement
+
 import stat
 import sys
 import os
@@ -110,18 +113,18 @@ def copytree(src, dst, symlinks=False, ignore=None, hardlinks=False):
             else:
                 copy(srcname, dstname)
         # XXX What about devices, sockets etc.?
-        except (IOError, os.error) as why:
+        except (IOError, os.error), why:
             errors.append((srcname, dstname, str(why)))
         # catch the Error from the recursive copytree so that we can
         # continue with other files
-        except shutil.Error as err:
+        except shutil.Error, err:
             errors.extend(err.args[0])
     try:
         shutil.copystat(src, dst)
     except shutil.WindowsError:
         # can't copy file access times on Windows
         pass
-    except OSError as why:
+    except OSError, why:
         errors.extend((src, dst, str(why)))
     if errors:
         raise shutil.Error(errors)
