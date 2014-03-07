3829bf5743c01f166ee239884a137d01aa6adc25
Allan Johns <ajohns@nn188.methodstudios.com>
2014-03-06 17:28:36 -0800

    -minor msg change

--- src/rez/cli/bootstrap.py
+++ src/rez/cli/bootstrap.py
@@ -1,4 +1,5 @@
 import sys
+import os.path
 from rez.bootstrap import print_info, install_into, is_bootstrapped, \
     is_in_virtualenv
 
@@ -15,8 +16,8 @@ def command(opts, parser=None):
 
         init_script = install_into(opts.install_path, opts.shell)
         print
-        print ("Rez has been packaged into a self-contained installation. To " + \
-        "bind Rez to the current environment, source the file %s.") % init_script
+        print ("Rez has been bootstrapped into %s. To bind Rez to the current " + \
+        "environment, source the file %s." % (opts.install_path, init_script))
         print
         print "Do not move this installation to another location. Instead, " + \
         "run 'rez-bootstrap --install-path=PATH' again, specifying the new " + \
