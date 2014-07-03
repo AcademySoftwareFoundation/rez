7a6fdcab6667b89ec63e12bf9f2a119db31e6336
mylene pepe <mylene.pepe@methodstudios.com>
2014-07-03 13:00:28 -0700

    * added quiet and on_error settings to control the output level and what to do in case of command execution error * replaced 'sudo' with 'user' where user=='root' is equivalent to sudo==True; this also allows to run commands as a specific user

--- src/rezplugins/release_hook/command.py
+++ src/rezplugins/release_hook/command.py
@@ -1,13 +1,15 @@
 from rez.release_hook import ReleaseHook
-import sh
-import sys
+from rez.vendor.sh.sh import Command, ErrorReturnCode, sudo, which
+import sys, os
 
 
 class CommandReleaseHook(ReleaseHook):
 
     schema_dict = {
-        "pre_commands":     [(basestring,[basestring],bool)],
-        "post_commands":    [(basestring,[basestring],bool)]}
+        "quiet":            bool,
+        "on_error":         Or('stop', 'ignore', 'raise'),
+        "pre_commands":     [(basestring,[basestring],Or(None, basestring))],
+        "post_commands":    [(basestring,[basestring],Or(None, basestring))]}
 
     @classmethod
     def name(cls):
@@ -16,30 +18,49 @@ class CommandReleaseHook(ReleaseHook):
     def __init__(self, source_path):
         super(CommandReleaseHook, self).__init__(source_path)
 
-    def execute_command(self, cmd_full_path, cmd_arguments=[], cmd_sudo=False):
-        run_cmd = sh.Command(cmd_full_path)
-        def _execute_cmd_private(cmd, arguments):
+    def execute_command(self, cmd_name, cmd_arguments=[], user=None):
+        def _execute_cmd_private(cmd_full_path, arguments, on_error='ignore', quiet=False):
+            error_class = None if on_error == 'raise' else ErrorReturnCode
             try:
-                result = cmd(arguments)
-                print result.stdout.strip()
-            except sh.ErrorReturnCode, err:
-                print >> sys.stderr, err.stderr.strip()
+                result = cmd_full_path(arguments)
+                if not quiet:
+                    print result.stdout.strip()
+            except error_class as err:
+                if not quiet:
+                    print >> sys.stderr, err.stderr.strip()
+                if on_error == 'stop':
+                    return 'bail'
+            return 'proceed'
 
-        if cmd_sudo:
-            with sh.sudo:
-                _execute_cmd_private(run_cmd, cmd_arguments)
+        if not os.path.isfile(cmd_name):
+            cmd_full_path = which(cmd_name)
         else:
-            _execute_cmd_private(run_cmd, cmd_arguments)
+            cmd_full_path = cmd_name
+
+        cmd_env = os.environ.copy()
+        if user not in (None, 'root'):
+            cmd_env['USER'] = user
+
+        run_cmd = Command(cmd_full_path, env=cmd_env)
+
+        settings = self.package.config.plugins.release_hook.command
+        if user == 'root':
+            with sudo:
+                _execute_cmd_private(run_cmd, cmd_arguments, settings.on_error, settings.quiet)
+        else:
+            _execute_cmd_private(run_cmd, cmd_arguments, settings.on_error, settings.quiet)
 
     def pre_release(self):
         settings = self.package.config.plugins.release_hook.command
-        for (cmd_full_path, cmd_arguments, cmd_sudo) in settings.pre_commands:
-            self.execute_command(cmd_full_path, cmd_arguments, cmd_sudo)
+        for (cmd_name, cmd_arguments, user) in settings.pre_commands:
+            if self.execute_command(cmd_name, cmd_arguments, user) == 'bail':
+                return
 
     def post_release(self):
         settings = self.package.config.plugins.release_hook.command
-        for (cmd_full_path, cmd_arguments, cmd_sudo) in settings.post_commands:
-            self.execute_command(cmd_full_path, cmd_arguments, cmd_sudo)
+        for (cmd_name, cmd_arguments, user) in settings.post_commands:
+            if self.execute_command(cmd_name, cmd_arguments, user) == 'bail':
+                return
 
 
 def register_plugin():
