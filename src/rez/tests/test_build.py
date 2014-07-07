3268860b0e1bc56346d3ea5eb9c85d891acabf25
Mark Streatfield <markst@al.com.au>
2014-07-07 14:25:51 +1000

    + Update based on changes to build process.

--- src/rez/tests/test_build.py
+++ src/rez/tests/test_build.py
@@ -5,6 +5,7 @@ import rez.vendor.unittest2 as unittest
 from rez.tests.util import TestBase, TempdirMixin, shell_dependent, \
     install_dependent
 from rez.settings import settings
+from rez.exceptions import BuildProcessContextResolveError
 import rez.bind.platform
 import rez.bind.arch
 import rez.bind.os
@@ -90,7 +91,7 @@ class TestBuild(TestBase, TempdirMixin):
         """Test that a package with conflicting requirements fails correctly."""
         working_dir = os.path.join(self.src_root, "loco", "3")
         builder = self._create_builder(working_dir)
-        self.assertFalse(builder.build(clean=True))
+        self.assertRaises(BuildProcessContextResolveError, builder.build, clean=True)
 
     def _test_build_bah(self):
         """Build, install, test the bah package."""
