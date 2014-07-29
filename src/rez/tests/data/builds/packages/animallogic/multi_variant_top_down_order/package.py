3eb31a88daae439ef923a525894cb82460649a3e
Federico Naum <federicon@al.com.au>
2014-07-29 19:06:10 +1000

    adding test cases

---
+++ src/rez/tests/data/builds/packages/animallogic/multi_variant_top_down_order/package.py
@@ -0,0 +1,10 @@
+name = 'multi_variant_bottom_up_order'
+version = '1'
+authors = ["oops"]
+uuid = "ed8a45e3-233e-4341-8ba9-9d53c470658c"
+description = "package with several variants of bar"
+variants = [
+    ["bar-4.5.3"],
+    ["bar-4.8.2"],
+    ["bar-4.8.5"]]
+
--- src/rez/tests/test_animallogic.py
+++ src/rez/tests/test_animallogic.py
@@ -14,93 +14,174 @@ import os
 import shutil
 
 
-class TestConvertingOldStyleCommands(TestBase):
+# class TestConvertingOldStyleCommands(TestBase):
+#
+#     def test_old_style_cmake_module_path_commands_with_separator(self):
+#
+#         expected = "prependenv('CMAKE_MODULE_PATH', '{root}/cmake')"
+#
+#         command = "export CMAKE_MODULE_PATH=!ROOT!/cmake';'$CMAKE_MODULE_PATH"
+#         self.assertEqual(expected, convert_old_commands([command], annotate=False))
+#
+#         command = "export CMAKE_MODULE_PATH=!ROOT!/cmake:$CMAKE_MODULE_PATH"
+#         self.assertEqual(expected, convert_old_commands([command], annotate=False))
+#
+#         command = "export CMAKE_MODULE_PATH=!ROOT!/cmake;$CMAKE_MODULE_PATH"
+#         self.assertEqual(expected, convert_old_commands([command], annotate=False))
+#
+#         command = 'export CMAKE_MODULE_PATH=!ROOT!/cmake";"$CMAKE_MODULE_PATH'
+#         self.assertEqual(expected, convert_old_commands([command], annotate=False))
+#
+#     def test_old_style_commands_enclosed_in_quotes(self):
+#
+#         expected = "setenv('FOO', 'BAR SPAM')"
+#
+#         command = "export FOO='BAR SPAM'"
+#         self.assertEqual(expected, convert_old_commands([command], annotate=False))
+#
+#         command = 'export FOO="BAR SPAM"'
+#         self.assertEqual(expected, convert_old_commands([command], annotate=False))
+#
+#     def test_old_style_non_pathsep_commands(self):
+#
+#         test_separators = {"FOO":" ", "BAR":","}
+#         config.override("env_var_separators", test_separators)
+#
+#         command = "export FOO=!ROOT!/cmake $FOO"
+#         expected = "prependenv('FOO', '{root}/cmake')"
+#         self.assertEqual(expected, convert_old_commands([command], annotate=False))
+#
+#         command = "export BAR=!ROOT!/cmake,$BAR"
+#         expected = "prependenv('BAR', '{root}/cmake')"
+#         self.assertEqual(expected, convert_old_commands([command], annotate=False))
+#
+#
+# class TestRex(TestBase):
+#
+#     def test_non_pathsep_commands(self):
+#
+#         test_separators = {"FOO":" ", "BAR":","}
+#         config.override("env_var_separators", test_separators)
+#
+#         def _rex():
+#             prependenv("FOO", "spam")
+#             prependenv("FOO", "ham")
+#             prependenv("FOO", "chips")
+#             appendenv("BAR", "eggs")
+#             appendenv("BAR", "bacon")
+#             appendenv("BAR", "beans")
+#
+#         expected = {"FOO":"chips ham spam",
+#                     "BAR":"eggs,bacon,beans"}
+#
+#         executor = self._create_executor({})
+#         executor.execute_function(_rex)
+#         self.assertEqual(executor.get_output(), expected)
+#
+#     def _create_executor(self, env, **kwargs):
+#
+#         interp = Python(target_environ={}, passive=True)
+#
+#         return RexExecutor(interpreter=interp,
+#                            parent_environ=env,
+#                            bind_rez=False,
+#                            shebang=False,
+#                            **kwargs)
+#
+#
+# class TestVariantPathMunging(TestBase):
+#
+#     def test_safe_string(self):
+#
+#         self.assertEqual(Requirement("!foo").safe_str(), "_not_foo")
+#         self.assertEqual(Requirement("foo-5+<6").safe_str(), "foo-5_thru_6")
+#         self.assertEqual(Requirement("foo-5+").safe_str(), "foo-5_ge_")
+#         self.assertEqual(Requirement("foo-<5").safe_str(), "foo_lt_5")
+#         self.assertEqual(Requirement("~foo").safe_str(), "_weak_foo")
+#
+#
+# class TestCMakeBuildSystem(TestBase, TempdirMixin):
+#
+#     class FakeArgParseOpts(object):
+#
+#         def __getattr__(self, key):
+#             return None
+#
+#     @classmethod
+#     def setUpClass(cls):
+#         TempdirMixin.setUpClass()
+#
+#         path = os.path.dirname(__file__)
+#         packages_path = os.path.join(path, "data", "builds", "packages", "animallogic")
+#
+#         cls.src_root = os.path.join(cls.root, "src", "packages")
+#         cls.install_root = os.path.join(cls.root, "packages")
+#
+#         shutil.copytree(packages_path, cls.src_root)
+#
+#         cls.settings = dict(
+#             packages_path=[cls.install_root],
+#             add_bootstrap_path=False,
+#             resolve_caching=False,
+#             warn_untimestamped=False,
+#             implicit_packages=[])
+#
+#         working_dir = os.path.join(cls.src_root, "foo", "1.0.0")
+#         builder = cls._create_builder(working_dir)
+#         builder.build(install_path=cls.install_root, install=True, clean=True)
+#
+#         working_dir = os.path.join(cls.src_root, "foo", "1.1.0")
+#         builder = cls._create_builder(working_dir)
+#         builder.build(install_path=cls.install_root, install=True, clean=True)
+#
+#     @classmethod
+#     def tearDownClass(cls):
+#
+#         TempdirMixin.tearDownClass()
+#
+#     @classmethod
+#     def _create_builder(cls, working_dir):
+#         buildsys = create_build_system(working_dir)
+#         return LocalSequentialBuildProcess(working_dir,
+#                                            buildsys,
+#                                            vcs=None)
+#
+#     def test_multiple_build_systems_with_cmake(self):
+#
+#         working_dir = os.path.join(self.src_root, "multiple_build_systems_with_cmake")
+#         buildsys = create_build_system(working_dir, opts=TestCMakeBuildSystem.FakeArgParseOpts())
+#         self.assertEqual(buildsys.name(), "cmake")
+#
+#     def test_multiple_build_systems_without_cmake(self):
+#
+#         working_dir = os.path.join(self.src_root, "multiple_build_systems_without_cmake")
+#         self.assertRaises(BuildSystemError, create_build_system, working_dir, opts=TestCMakeBuildSystem.FakeArgParseOpts())
+#
+#     def test_current_variant_index_with_variants(self):
+#
+#         working_dir = os.path.join(self.src_root, "current_variant_index_with_variants")
+#         package = load_developer_package(working_dir)
+#
+#         variants = list(package.iter_variants())
+#         self.assertEqual(len(variants), 2)
+#
+#         for i, variant in enumerate(variants):
+#             request = variant.get_requires(build_requires=True, private_build_requires=True)
+#             context = ResolvedContext(request, building=True)
+#             self.assertEqual(get_current_variant_index(context, package), i)
+#
+#     def test_current_variant_index_without_variants(self):
+#
+#         working_dir = os.path.join(self.src_root, "current_variant_index_without_variants")
+#         package = load_developer_package(working_dir)
+#
+#         for i, variant in enumerate(package.iter_variants()):
+#             self.assertEqual(i, 0)
+#             request = variant.get_requires(build_requires=True, private_build_requires=True)
+#             context = ResolvedContext(request, building=True)
+#             self.assertEqual(get_current_variant_index(context, package), 0)
 
-    def test_old_style_cmake_module_path_commands_with_separator(self):
-
-        expected = "prependenv('CMAKE_MODULE_PATH', '{root}/cmake')"
-
-        command = "export CMAKE_MODULE_PATH=!ROOT!/cmake';'$CMAKE_MODULE_PATH"
-        self.assertEqual(expected, convert_old_commands([command], annotate=False))
-
-        command = "export CMAKE_MODULE_PATH=!ROOT!/cmake:$CMAKE_MODULE_PATH"
-        self.assertEqual(expected, convert_old_commands([command], annotate=False))
-
-        command = "export CMAKE_MODULE_PATH=!ROOT!/cmake;$CMAKE_MODULE_PATH"
-        self.assertEqual(expected, convert_old_commands([command], annotate=False))
-
-        command = 'export CMAKE_MODULE_PATH=!ROOT!/cmake";"$CMAKE_MODULE_PATH'
-        self.assertEqual(expected, convert_old_commands([command], annotate=False))
-
-    def test_old_style_commands_enclosed_in_quotes(self):
-
-        expected = "setenv('FOO', 'BAR SPAM')"
-
-        command = "export FOO='BAR SPAM'"
-        self.assertEqual(expected, convert_old_commands([command], annotate=False))
-
-        command = 'export FOO="BAR SPAM"'
-        self.assertEqual(expected, convert_old_commands([command], annotate=False))
-
-    def test_old_style_non_pathsep_commands(self):
-
-        test_separators = {"FOO":" ", "BAR":","}
-        config.override("env_var_separators", test_separators)
-
-        command = "export FOO=!ROOT!/cmake $FOO"
-        expected = "prependenv('FOO', '{root}/cmake')"
-        self.assertEqual(expected, convert_old_commands([command], annotate=False))
-
-        command = "export BAR=!ROOT!/cmake,$BAR"
-        expected = "prependenv('BAR', '{root}/cmake')"
-        self.assertEqual(expected, convert_old_commands([command], annotate=False))
-
-
-class TestRex(TestBase):
-
-    def test_non_pathsep_commands(self):
-
-        test_separators = {"FOO":" ", "BAR":","}
-        config.override("env_var_separators", test_separators)
-
-        def _rex():
-            prependenv("FOO", "spam")
-            prependenv("FOO", "ham")
-            prependenv("FOO", "chips")
-            appendenv("BAR", "eggs")
-            appendenv("BAR", "bacon")
-            appendenv("BAR", "beans")
-
-        expected = {"FOO":"chips ham spam",
-                    "BAR":"eggs,bacon,beans"}
-
-        executor = self._create_executor({})
-        executor.execute_function(_rex)
-        self.assertEqual(executor.get_output(), expected)
-
-    def _create_executor(self, env, **kwargs):
-
-        interp = Python(target_environ={}, passive=True)
-
-        return RexExecutor(interpreter=interp,
-                           parent_environ=env,
-                           bind_rez=False,
-                           shebang=False,
-                           **kwargs)
-
-
-class TestVariantPathMunging(TestBase):
-
-    def test_safe_string(self):
-
-        self.assertEqual(Requirement("!foo").safe_str(), "_not_foo")
-        self.assertEqual(Requirement("foo-5+<6").safe_str(), "foo-5_thru_6")
-        self.assertEqual(Requirement("foo-5+").safe_str(), "foo-5_ge_")
-        self.assertEqual(Requirement("foo-<5").safe_str(), "foo_lt_5")
-        self.assertEqual(Requirement("~foo").safe_str(), "_weak_foo")
-
-
-class TestCMakeBuildSystem(TestBase, TempdirMixin):
+class TestVariantResolutionOrder(TestBase, TempdirMixin):
 
     class FakeArgParseOpts(object):
 
@@ -114,10 +195,7 @@ class TestCMakeBuildSystem(TestBase, TempdirMixin):
         path = os.path.dirname(__file__)
         packages_path = os.path.join(path, "data", "builds", "packages", "animallogic")
 
-        cls.src_root = os.path.join(cls.root, "src", "packages")
         cls.install_root = os.path.join(cls.root, "packages")
-
-        shutil.copytree(packages_path, cls.src_root)
 
         cls.settings = dict(
             packages_path=[cls.install_root],
@@ -126,66 +204,55 @@ class TestCMakeBuildSystem(TestBase, TempdirMixin):
             warn_untimestamped=False,
             implicit_packages=[])
 
-        working_dir = os.path.join(cls.src_root, "foo", "1.0.0")
-        builder = cls._create_builder(working_dir)
-        builder.build(install_path=cls.install_root, install=True, clean=True)
-
-        working_dir = os.path.join(cls.src_root, "foo", "1.1.0")
-        builder = cls._create_builder(working_dir)
-        builder.build(install_path=cls.install_root, install=True, clean=True)
+        shutil.copytree(packages_path, cls.install_root)
 
     @classmethod
     def tearDownClass(cls):
-
         TempdirMixin.tearDownClass()
 
-    @classmethod
-    def _create_builder(cls, working_dir):
-        buildsys = create_build_system(working_dir)
-        return LocalSequentialBuildProcess(working_dir,
-                                           buildsys,
-                                           vcs=None)
+    def test_resolve_bottom_up_order_variants(self):
+        """
+        Test we pick up the higher version of the dependant package when the variants are ordered bottom up
+        """
+        resolved_package_version = None
+        bar_path = os.path.join(self.install_root, "bar", "4.8.5")
+        bar_package = load_developer_package(bar_path)
+        expected_package_version = bar_package.version
 
-    def test_multiple_build_systems_with_cmake(self):
+        request = ['multi_variant_bottom_up_order']
+        context = ResolvedContext(request, building=True)
+        for package in context.resolved_packages:
+            if package.name == 'bar':
+                resolved_package_version = package.version
+                break
 
-        working_dir = os.path.join(self.src_root, "multiple_build_systems_with_cmake")
-        buildsys = create_build_system(working_dir, opts=TestCMakeBuildSystem.FakeArgParseOpts())
-        self.assertEqual(buildsys.name(), "cmake")
+        self.assertEqual(resolved_package_version, expected_package_version)
 
-    def test_multiple_build_systems_without_cmake(self):
+    def test_resolve_top_down_order_variants(self):
+        """
+        Test we pick up the higher version of the dependant package when the variants are ordered top dow
+        """
 
-        working_dir = os.path.join(self.src_root, "multiple_build_systems_without_cmake")
-        self.assertRaises(BuildSystemError, create_build_system, working_dir, opts=TestCMakeBuildSystem.FakeArgParseOpts())
+        resolved_package_version = None
+        bar_path = os.path.join(self.install_root, "bar", "4.8.5")
+        bar_package = load_developer_package(bar_path)
+        expected_package_version = bar_package.version
 
-    def test_current_variant_index_with_variants(self):
+        request = ['multi_variant_top_down_order']
+        context = ResolvedContext(request, building=True)
+        for package in context.resolved_packages:
+            if package.name == 'bar':
+                resolved_package_version = package.version
+                break
 
-        working_dir = os.path.join(self.src_root, "current_variant_index_with_variants")
-        package = load_developer_package(working_dir)
-
-        variants = list(package.iter_variants())
-        self.assertEqual(len(variants), 2)
-
-        for i, variant in enumerate(variants):
-            request = variant.get_requires(build_requires=True, private_build_requires=True)
-            context = ResolvedContext(request, building=True)
-            self.assertEqual(get_current_variant_index(context, package), i)
-
-    def test_current_variant_index_without_variants(self):
-
-        working_dir = os.path.join(self.src_root, "current_variant_index_without_variants")
-        package = load_developer_package(working_dir)
-
-        for i, variant in enumerate(package.iter_variants()):
-            self.assertEqual(i, 0)
-            request = variant.get_requires(build_requires=True, private_build_requires=True)
-            context = ResolvedContext(request, building=True)
-            self.assertEqual(get_current_variant_index(context, package), 0)
+        self.assertEqual(resolved_package_version, expected_package_version)
 
 
 def get_test_suites():
 
     suites = []
-    tests = [TestConvertingOldStyleCommands, TestRex, TestVariantPathMunging, TestCMakeBuildSystem]
+    tests = [TestConvertingOldStyleCommands, TestRex, TestVariantPathMunging, TestCMakeBuildSystem, TestVariantResolutionOrder]
+    tests = [TestVariantResolutionOrder]
 
     for test in tests:
         suites.append(unittest.TestLoader().loadTestsFromTestCase(test))
