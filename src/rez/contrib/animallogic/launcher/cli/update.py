1e9ced67b3a40d82830ab79e475e93860e2faaab
Mark Streatfield <markst@al.com.au>
2014-10-16 10:31:03 +1100

    + Remove some unused print statements, tweaked the cli descriptions slightly.

--- src/rez/contrib/animallogic/launcher/cli/update.py
+++ src/rez/contrib/animallogic/launcher/cli/update.py
@@ -9,16 +9,15 @@ from rez.contrib.animallogic.launcher.service import LauncherHessianService
 from rez.config import config
 
 
-
 def setup_parser(parser):
 
     parser.add_argument("--add-reference",  action='append',
-                           help="the reference launcher path to replace in the destination preset.")
+        help="Add a new reference to the preset.")
     parser.add_argument("--remove-all-references", action='store_true', default=False,
-                         help="Remove all references to preset from the target preset")
+        help="Remove all references from the target preset.")
     parser.add_argument("--description",
-                        help="a description for the reference change.")
-    parser.add_argument('target', help='the target preset where the updates are going to be applied ')
+        help="A description for the preset change.")
+    parser.add_argument('target', help='The target preset where the updates are going to be applied.')
 
 
 def command(opts, parser, extra_arg_groups=None):
@@ -30,12 +29,8 @@ def command(opts, parser, extra_arg_groups=None):
 
     if not description:
         description = "Preset automatically updated by Rez. The command used was %s" % " ".join(sys.argv)
-    print target_preset
-    print reference_list
-    print description
-    print remove_all_references
 
-    update(target_preset, reference_list, description,remove_all_references)
+    update(target_preset, reference_list, description, remove_all_references)
 
 
 def update(target, reference_list, description, remove_all_references, ):
