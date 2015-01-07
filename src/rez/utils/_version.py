37f43999140a6ba8883e50446d5b331341ed5439
Allan Johns <ajohns@la-ws024.methodstudios.com>
2015-01-07 10:55:40 -0800

    -added 'lockfile' vendor module -added file locking to filesystem pkg repo install_variant()

--- src/rez/utils/_version.py
+++ src/rez/utils/_version.py
@@ -1,4 +1,4 @@
 
 
-# Update this value to version up Rez. Do not plaec anything else in this file.
-_rez_version = "2.0.BETA.16"
+# Update this value to version up Rez. Do not place anything else in this file.
+_rez_version = "2.0.BETA.30"
