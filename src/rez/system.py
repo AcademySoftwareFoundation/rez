dce3a1d46eec21e7652a45b76434c22386443dc5
Robert Minsk <robert.minsk@methodstudios.com>
2015-03-09 02:38:34 -0700

    Added home directory property

--- src/rez/system.py
+++ src/rez/system.py
@@ -203,6 +203,13 @@ class System(object):
 
         return None
 
+    @cached_property
+    def home(self):
+        """
+	@returns The home directory of the current user
+	"""
+        return os.path.expanduser('~')
+
     @property
     def is_production_rez_install(self):
         """Return True if this is a production rez install."""
