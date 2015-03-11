7078aca26db85dd4811c3f792da901b7d53dcc03
Federico Naum <federicon@al.com.au>
2015-03-11 12:04:59 +1100

    Adding a check so releases can only happen from the rnd organization. Releases from a fork are not allowed

--- src/rezplugins/release_vcs/github.py
+++ src/rezplugins/release_vcs/github.py
@@ -1,8 +1,6 @@
-import commands
-from rez.config import Str
+import re
 from rez.exceptions import ReleaseVCSError
 from rezplugins.release_vcs.git import GitReleaseVCS
-from rez import config
 
 
 class GitHubReleaseVCSError(ReleaseVCSError):
@@ -15,32 +13,29 @@ class GitHubReleaseVCS(GitReleaseVCS):
     def name(cls):
         return 'github'
 
-
     def validate_repostate(self):
-        # Check that the repository is not a fork and belongs to the white listed organizations
         super(GitHubReleaseVCS, self).validate_repostate()
-        org = self._get_organization()
-        if org not in self.type_settings.releasable_organizations:
-            raise GitHubReleaseVCSError("GitHub organization %s not among the allowed organizations %s"
-                                        % (org, self.type_settings.releasable_organizations))
+
+        # Check that the repository is not a fork and belongs to the white listed organizations
+        current_organization = self._get_organization()
+        releasable = False
+        for organization in self.type_settings.releasable_organizations:
+            if re.search(organization, current_organization):
+                releasable = True
+
+        if not releasable:
+            raise GitHubReleaseVCSError("GitHub organization %s is not among the allowed organizations %s"
+                                        % (current_organization, self.type_settings.releasable_organizations))
 
     def _get_remote_origin_url(self):
-        return self.git('config', 'remote.origin.url')
+        return self.git('config', 'remote.origin.url')[0]
 
     def _get_organization(self):
-        # Assume we always have http://DOMAIN/ORG/REPO
+        # Assumes we always have http://DOMAIN/ORG/REPO
         try:
             return self._get_remote_origin_url().split('/')[-2]
         except IndexError:
             return GitHubReleaseVCSError("Could not retrieve the GiHub organization from the origin remote: %s "
-                                         % self._get_remote_origin_url())
-
-    def _get_remote_origin_domain_name(self):
-        # Assume we always have http://DOMAIN/ORG/REPO
-        try:
-            return self._get_remote_origin_url().split('/')[2]
-        except IndexError:
-            return GitHubReleaseVCSError("Could not retrieve the GiHub origin remote domain name from: %s "
                                          % self._get_remote_origin_url())
 
 
