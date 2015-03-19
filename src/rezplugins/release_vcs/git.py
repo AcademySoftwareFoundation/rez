7af05b25716acd68f4ce5b7ea93bc132bcc349d8
Mark Streatfield <markst@al.com.au>
2015-03-19 11:36:32 +1100

    + Post cherrypick changes.

--- src/rezplugins/release_vcs/git.py
+++ src/rezplugins/release_vcs/git.py
@@ -5,7 +5,7 @@ import commands
 from rez.release_vcs import ReleaseVCS
 from rez.utils.logging_ import print_error, print_warning, print_debug
 from rez.exceptions import ReleaseVCSError
-from rez.config import Str, config
+from rez.config import config
 import functools
 import subprocess
 import re
