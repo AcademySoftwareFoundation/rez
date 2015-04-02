7af2dcdbbf9829b110d1cc4695dbc37a64ad87f4
Mark Streatfield <markst@al.com.au>
2015-04-02 16:38:42 +1100

    + Stop leaking variables.

--- src/rez/tests/test_config.py
+++ src/rez/tests/test_config.py
@@ -114,9 +114,14 @@ class TestConfig(TestBase):
         self._test_basic(c)
 
         # test env-var override that contains a system expansion
+        original = os.environ.get("REZ_TMPDIR")
         os.environ["REZ_TMPDIR"] = "/tmp/{system.user}"
         expected_value = "/tmp/%s" % system.user
         self.assertEqual(c.tmpdir, expected_value)
+        if original is None:
+            del os.environ["REZ_TMPDIR"]
+        else:
+            os.environ["REZ_TMPDIR"] = original
 
         # _test_overrides overrides this value, so here we're making sure
         # that an API override takes precedence over an env-var override
