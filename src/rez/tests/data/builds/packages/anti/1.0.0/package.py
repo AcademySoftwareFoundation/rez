c1824379f7de3cfe57581ae46c311453b103b142
Mark Streatfield <markst@al.com.au>
2015-04-21 09:23:49 +1000

    + Add missing test data.

---
+++ src/rez/tests/data/builds/packages/anti/1.0.0/package.py
@@ -0,0 +1,12 @@
+name = 'anti'
+version = '1.0.0'
+authors = ["joe.bloggs"]
+uuid = "e760fa04-043d-47bb-ba4d-543b18a70959"
+description = "package with anti package"
+
+
+private_build_requires = ["build_util"]
+requires = ["floob", "!loco"]
+
+def commands():
+    env.PYTHONPATH.append('{root}/python')
