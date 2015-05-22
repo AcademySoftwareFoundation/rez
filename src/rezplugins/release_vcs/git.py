9c0586b6a2f0d14db0cc959761b78b18a06fc36f
Mark Streatfield <markst@al.com.au>
2015-05-22 12:38:24 +1000

    Various improvements to git vcs repository.
    * Handle the case where we might be both ahead of and behind the remote.
    * Fix docstrings.
    * Add a check for untracked files.  This was in rez 1 although does make
      things more strict
    * Handle if the changelog is broken - if history is rewritten or the
      repository moves (in our case git to github) the changelog can fail
      ungracefully.
    * Don't let the export function leave you in a different directory to
      which you started - that is bad for tests etc.
    
    # Conflicts:
    #	src/rezplugins/release_vcs/git.py

--- src/rezplugins/release_vcs/git.py
+++ src/rezplugins/release_vcs/git.py
@@ -46,7 +46,8 @@ class GitReleaseVCS(ReleaseVCS):
 
     def get_relative_to_remote(self):
         """Return the number of commits we are relative to the remote. Negative
-        is behind, positive in front, zero means we are matched to remote.
+        is behind, positive in front, zero means we are matched to remote.  if
+        we are both behind and ahead then only the ahead value will be reported.
         """
         s = self.git("status", "--short", "-b")[0]
         r = re.compile("\[([^\]]+)\]")
@@ -54,7 +55,7 @@ class GitReleaseVCS(ReleaseVCS):
         if toks:
             try:
                 s2 = toks[-1]
-                adj, n = s2.split()
+                adj, n = s2.split(",")[0].split()
                 assert(adj in ("ahead", "behind"))
                 n = int(n)
                 return -n if adj == "behind" else n
@@ -70,7 +71,8 @@ class GitReleaseVCS(ReleaseVCS):
         return self.git("rev-parse", "--abbrev-ref", "HEAD")[0]
 
     def get_tracking_branch(self):
-        """Returns (remote, branch) tuple, or None,None if there is no remote.
+        """Returns (remote, branch) tuple, or (None, None) if there is no
+        remote.
         """
         try:
             remote_uri = self.git("rev-parse", "--abbrev-ref",
@@ -116,6 +118,13 @@ class GitReleaseVCS(ReleaseVCS):
                     "one of: %s"
                     % (current_branch_name, ', '.join(releasable_branches)))
 
+        # check for untracked files
+        output = self.git("ls-files", "--other", "--exclude-standard")
+        if output:
+            msg = "Could not release: there are untracked files:\n"
+            msg += '\n'.join(output)
+            raise ReleaseVCSError(msg)
+
         # check for uncommitted changes
         try:
             self.git("diff-index", "--quiet", "HEAD")
@@ -127,7 +136,7 @@ class GitReleaseVCS(ReleaseVCS):
 
         # check if we are behind/ahead of remote
         if remote:
-            self.git("remote", "update")
+            self.git("remote", "update", remote)
             n = self.get_relative_to_remote()
             if n:
                 s = "ahead of" if n > 0 else "behind"
@@ -152,10 +161,15 @@ class GitReleaseVCS(ReleaseVCS):
         if prev_commit:
             # git returns logs to last common ancestor, so even if previous
             # release was from a different branch, this is ok
-            commit_range = "%s..HEAD" % prev_commit
-            args.append(commit_range)
-        stdout = self.git(*args)
-
+            try:
+                commit_range = "%s..HEAD" % prev_commit
+                stdout = self.git("log", commit_range)
+            except ReleaseVCSError:
+                # Special case where the sha stored in the latest version does not exists due to the
+                # git ->github migration where we rewrote the history of the repos
+                stdout = self.git("log")
+        else:
+            stdout = self.git("log")
         return '\n'.join(stdout)
 
     def get_current_revision(self):
@@ -219,6 +233,7 @@ class GitReleaseVCS(ReleaseVCS):
 
     @classmethod
     def export(cls, revision, path):
+        cwd = os.getcwd()
         url = revision["fetch_url"]
         commit = revision["commit"]
         path_, dirname = os.path.split(path)
