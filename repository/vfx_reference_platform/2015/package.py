7f582bb246b0460883ea39fbe23f36694f7db806
Allan Johns <ajohns@la-ws012.methodstudios.com>
2015-07-27 18:06:25 -0700

    -start of reference package repository

---
+++ repository/vfx_reference_platform/2015/package.py
@@ -0,0 +1,30 @@
+name = "vfx_reference_platform"
+
+version = "2015"
+
+authors = [
+    "ajohns"
+]
+
+description = \
+    """
+    Bundle for VFX Reference Platform.
+    """
+
+requires = [
+    "~gcc-4.8.2",
+    "~glibc-2.12",
+    "~python-2.7",
+    "~Qt-4.8",
+    "~PySide-1.2",
+    "~OpenEXR-2.2",
+    "~OpenSubDiv-2.5",
+    "~OpenVDB-3.0",
+    "~Alembic-1.5",
+    "~FBX",
+    "~OpenColorIO-1.0.9",
+    "~boost-1.55",
+    "~tbb-4.2"
+]
+
+uuid = "repository.vfx_reference_platform"
