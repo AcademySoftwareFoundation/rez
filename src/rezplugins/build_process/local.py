936767e36720351a0bbea855935b45d5a0e0b62f
ajohns <allan.johns@methodstudios.com>
2017-02-28 13:27:04 +1100

    -minor change, moved copy_and_replace() to rez.utils.filesystem

--- src/rezplugins/build_process/local.py
+++ src/rezplugins/build_process/local.py
@@ -7,7 +7,7 @@ from rez.build_process_ import BuildProcessHelper, BuildType
 from rez.release_hook import ReleaseHookEvent
 from rez.exceptions import BuildError, ReleaseError
 from rez.utils.colorize import Printer, warning
-from rez.utils.filesystem import safe_makedirs
+from rez.utils.filesystem import safe_makedirs, copy_or_replace
 from rez.utils.sourcecode import IncludeModuleManager
 from hashlib import sha1
 import shutil
@@ -144,38 +144,6 @@ class LocalBuildProcess(BuildProcessHelper):
 
         if not build_result.get("success"):
             raise BuildError("The %s build system failed" % build_system_name)
-
-        def copy_or_replace(src, dst):
-            '''try to copy with mode, and if it fails, try replacing
-            '''
-            try:
-                shutil.copy(src, dst)
-            except (OSError, IOError), e:
-                # It's possible that the file existed, but was owned by someone
-                # else - in that situation, shutil.copy might then fail when it
-                # tries to copy perms.
-                # However, it's possible that we have write perms to the dir -
-                # in which case, we can just delete and replace
-                import errno
-                if e.errno == errno.EPERM:
-                    import tempfile
-                    # try copying into a temporary location beside the old
-                    # file - if we have perms to do that, we should have perms
-                    # to then delete the old file, and move the new one into
-                    # place
-                    if os.path.isdir(dst):
-                        dst = os.path.join(dst, os.path.basename(src))
-
-                    dst_dir, dst_name = os.path.split(dst)
-                    dst_temp = tempfile.mktemp(prefix=dst_name + '.',
-                                               dir=dst_dir)
-                    shutil.copy(src, dst_temp)
-                    if not os.path.isfile(dst_temp):
-                        raise RuntimeError(
-                            "shutil.copy completed successfully, but path"
-                            " '%s' still did not exist" % dst_temp)
-                    os.remove(dst)
-                    shutil.move(dst_temp, dst)
 
         if install:
             # install some files for debugging purposes
