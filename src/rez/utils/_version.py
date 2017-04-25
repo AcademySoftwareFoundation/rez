e79bcae122b3b1e402004d1649a087652d16ddfd
Thorsten Kaufmann <Thorsten.Kaufmann@mackevision.com>
2017-04-25 12:17:57 +0200

    Bump version

--- src/rez/utils/_version.py
+++ src/rez/utils/_version.py
@@ -1,7 +1,7 @@
 
 
 # Update this value to version up Rez. Do not place anything else in this file.
-_rez_version = "2.13.0"
+_rez_version = "2.13.0.mv1"
 
 try:
     from rez.vendor.version.version import Version
