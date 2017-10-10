539d35876b1a5dd333d27b7bb0ec92f06c7fae3d
Thorsten Kaufmann <Thorsten.Kaufmann@mackevision.com>
2017-10-10 11:58:03 +0200

    Remove sh module from vendored packages

--- src/rez/vendor/sh/__init__.py
+++
@@ -1,6 +0,0 @@
-# putting rez-specific code here, because this file wouldn't exist in a
-# "normal" distribution of sh
-
-from rez.config import config
-from . import sh
-sh.ErrorReturnCode.truncate_cap = config.shell_error_truncate_cap
