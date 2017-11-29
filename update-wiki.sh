36d78e67dcd53a088b00762dce16ea83d43ddb14
ajohns <allan.johns@methodstudios.com>
2017-11-29 16:02:44 +1100

    added wiki update script

---
+++ update-wiki.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# This script:
+# 1. Takes the content from here: https://github.com/nerdvegas/rez-wiki;
+# 2. Then writes it into a local clone of https://github.com/nerdvegas/rez.wiki.git;
+# 3. Then follows the procedure outlined in README from 2.
+#
+# This process exists because GitHub does not support contributions to wiki
+# repositories - this is a workaround.
+#
+set -e
+
+rm -rf .rez-gen-wiki-tmp
+mkdir .rez-gen-wiki-tmp
+cd .rez-gen-wiki-tmp
+
+git clone git@github.com:nerdvegas/rez.git
+git clone git@github.com:nerdvegas/rez-wiki.git
+git clone git@github.com:nerdvegas/rez.wiki.git
+
+cp ./rez-wiki/pages/* ./rez.wiki/pages/
+export REZ_SOURCE_DIR=$(pwd)/rez
+
+cd ./rez.wiki
+python ./utils/process.py
+bash ./utils/update.sh
+
+rm -rf .rez-gen-wiki-tmp
