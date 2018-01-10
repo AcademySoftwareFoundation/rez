ebe18cb2d9284bcc16eb619aacf84029fd3cd974
ajohns <allan.johns@methodstudios.com>
2018-01-10 15:53:16 +1100

    version up

--- src/rez/utils/_version.py
+++ src/rez/utils/_version.py
@@ -1,7 +1,7 @@
 
 
 # Update this value to version up Rez. Do not place anything else in this file.
-_rez_version = "2.17.1"
+_rez_version = "2.18.0"
 
 try:
     from rez.vendor.version.version import Version
