be008c56edc73bbc8ddf6320f1771999e3179adc
Sebastian Kral <sebastian.kral@pixomondo.com>
2018-02-26 15:41:13 -0500

    Fix doskey not available in PATH; got lost during a merge

--- src/rezplugins/shell/cmd.py
+++ src/rezplugins/shell/cmd.py
@@ -274,7 +274,19 @@ class CMD(Shell):
         self._addline(self.setenv(key, value))
 
     def alias(self, key, value):
-        self._addline("doskey %s=%s" % (key, value))
+        # the PATH may have been cleared prior to this command being run, but
+        # the doskey command should always be on the system paths recorded in
+        # the registry; lazy load those and then find the doskey.exe path,
+        # falling back on letting the shell search for it if that fails
+        if self._doskey is None:
+            doskey = 'doskey'
+            for p in self.get_syspaths():
+                f = os.path.normpath(os.path.join(p, doskey + '.exe'))
+                if os.path.isfile(f):
+                    doskey = f
+                    break
+            self._doskey = doskey
+        self._addline("%s %s=%s" % (self._doskey, key, value))
 
     def comment(self, value):
         for line in value.split('\n'):
