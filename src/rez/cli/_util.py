0bbfa4fde771db7f85d326c5a546668bbe255e7f
marcus <konstruktion@gmail.com>
2019-04-26 13:22:39 +0100

    Handle CTRL+C on Windows The below happens at the moment:   File "..\lib\site-packages\rez-2.29.0-py2.7.egg\rez\cli\_util.py", line 138, in sigbase_handler     os.kill(os.getpid(0), signal.CTRL_C_EVENT) TypeError: getpid() takes no arguments (1 given)

--- src/rez/cli/_util.py
+++ src/rez/cli/_util.py
@@ -135,7 +135,7 @@ def sigbase_handler(signum, frame):
     # FIXME this kills parent procs as well
     if not _env_var_true("_REZ_NO_KILLPG"):
         if os.name == "nt":
-            os.kill(os.getpid(0), signal.CTRL_C_EVENT)
+            os.kill(os.getpid(), signal.CTRL_C_EVENT)
         else:
             os.killpg(os.getpgid(0), signum)
     sys.exit(1)
