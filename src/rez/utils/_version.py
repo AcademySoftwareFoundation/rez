b283624cdbbcb51b4e0722e5814df73e97bbda93
marcus <konstruktion@gmail.com>
2019-04-28 20:18:52 +0100

    Increment version

--- src/rez/utils/_version.py
+++ src/rez/utils/_version.py
@@ -1,7 +1,7 @@
 
 
 # Update this value to version up Rez. Do not place anything else in this file.
-_rez_version = "2.29.3"
+_rez_version = "2.29.4"
 
 try:
     from rez.vendor.version.version import Version
