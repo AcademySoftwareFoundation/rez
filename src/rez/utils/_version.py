312bab357443ab17d6f8806815580a9c25b8712b
marcus <konstruktion@gmail.com>
2019-04-29 11:44:55 +0100

    Version bump

--- src/rez/utils/_version.py
+++ src/rez/utils/_version.py
@@ -1,7 +1,7 @@
 
 
 # Update this value to version up Rez. Do not place anything else in this file.
-_rez_version = "2.29.4"
+_rez_version = "2.29.5"
 
 try:
     from rez.vendor.version.version import Version
