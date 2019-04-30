4d968f3faf67070b7c58fe3b27453e90f75e424a
marcus <konstruktion@gmail.com>
2019-04-30 08:21:18 +0100

    Revert "Fetch PATH from current session"
    This reverts commit a164b75b4315e9112016d81c97fa5880f578d8c6.
    
    # Conflicts:
    #	src/rezplugins/shell/cmd.py

--- src/rezplugins/shell/cmd.py
+++ src/rezplugins/shell/cmd.py
@@ -11,6 +11,7 @@ from rez.backport.shutilwhich import which
 from functools import partial
 import os
 import re
+import subprocess
 
 
 class CMD(Shell):
@@ -75,7 +76,62 @@ class CMD(Shell):
             cls.syspaths = config.standard_system_paths
             return cls.syspaths
 
-        paths = os.getenv("PATH").split(os.pathsep)
+        # detect system paths using registry
+        def gen_expected_regex(parts):
+            whitespace = "[\s]+"
+            return whitespace.join(parts)
+
+        paths = []
+
+        cmd = [
+            "REG",
+            "QUERY",
+            "HKLM\\SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment",
+            "/v",
+            "PATH"
+        ]
+
+        expected = gen_expected_regex([
+            "HKEY_LOCAL_MACHINE\\\\SYSTEM\\\\CurrentControlSet\\\\Control\\\\Session Manager\\\\Environment",
+            "PATH",
+            "REG_(EXPAND_)?SZ",
+            "(.*)"
+        ])
+
+        p = popen(cmd, stdout=subprocess.PIPE,
+                  stderr=subprocess.PIPE, shell=True)
+        out_, _ = p.communicate()
+        out_ = out_.strip()
+
+        if p.returncode == 0:
+            match = re.match(expected, out_)
+            if match:
+                paths.extend(match.group(2).split(os.pathsep))
+
+        cmd = [
+            "REG",
+            "QUERY",
+            "HKCU\\Environment",
+            "/v",
+            "PATH"
+        ]
+
+        expected = gen_expected_regex([
+            "HKEY_CURRENT_USER\\\\Environment",
+            "PATH",
+            "REG_(EXPAND_)?SZ",
+            "(.*)"
+        ])
+
+        p = popen(cmd, stdout=subprocess.PIPE,
+                  stderr=subprocess.PIPE, shell=True)
+        out_, _ = p.communicate()
+        out_ = out_.strip()
+
+        if p.returncode == 0:
+            match = re.match(expected, out_)
+            if match:
+                paths.extend(match.group(2).split(os.pathsep))
 
         cls.syspaths = list(set([x for x in paths if x]))
 
