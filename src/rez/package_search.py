5d2d0b34a9b040760beee8c11ac641c15a244250
marcus <konstruktion@gmail.com>
2019-05-02 12:51:35 +0100

    Allow for packages which have no versions.

--- src/rez/package_search.py
+++ src/rez/package_search.py
@@ -65,31 +65,22 @@ def get_reverse_dependency_tree(package_name, depth=None, paths=None,
     lookup = defaultdict(set)
 
     for i, package_name_ in enumerate(package_names):
-        it = iter_packages(name=package_name_, paths=paths)
-        packages = list(it)
+        bar.next()
+        packages = list(iter_packages(name=package_name_, paths=paths))
         if not packages:
             continue
-
         pkg = max(packages, key=lambda x: x.version)
-        requires = []
 
-        for variant in pkg.iter_variants():
-            pbr = (private_build_requires and pkg.name == package_name)
-
-            requires += variant.get_requires(
-                build_requires=build_requires,
-                private_build_requires=pbr
-            )
+        requires = set(pkg.requires or [])
+        for req_list in (pkg.variants or []):
+            requires.update(req_list)
 
         for req in requires:
             if not req.conflict:
                 lookup[req.name].add(package_name_)
 
-        bar.next()
-
-    bar.finish()
-
     # perform traversal
+    bar.finish()
     n = 0
     consumed = set([package_name])
     working_set = set([package_name])
