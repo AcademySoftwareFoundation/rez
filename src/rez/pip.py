7c9e9189e29b9b8e9ee2871821f90a1f224ac907
marcus <konstruktion@gmail.com>
2019-05-11 18:46:51 +0100

    future - rez/pip.py

--- src/rez/pip.py
+++ src/rez/pip.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from rez.packages_ import get_latest_package
 from rez.vendor.version.version import Version
 from rez.vendor.distlib import DistlibException
@@ -392,7 +393,7 @@ def pip_install_package(source_name, python_version=None,
     python_exe, context = find_python(python_version)
     if context and config.debug("package_release"):
         buf = StringIO()
-        print >> buf, "\n\npackage download environment:"
+        print("\n\npackage download environment:", file=buf)
         context.print_info(buf)
         _log(buf.getvalue())
 
