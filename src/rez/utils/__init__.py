49a19b945c127ba6cc7b26ace2ded01c0366adfe
marcus <konstruktion@gmail.com>
2019-05-11 19:50:07 +0100

    Python 2 and 3 raise

--- src/rez/utils/__init__.py
+++ src/rez/utils/__init__.py
@@ -15,7 +15,7 @@ def reraise(exc, new_exc_cls=None, format_str=None):
     if format_str is None:
         format_str = "%s"
 
-    raise new_exc_cls, format_str % exc, sys.exc_info()[2]
+    raise new_exc_cls(format_str % exc, sys.exc_info()[2])
 
 
 # Copyright 2013-2016 Allan Johns.
