b37cb732ed018e74deda91e86c999962c909b396
lambdaclan <47409392+lambdaclan@users.noreply.github.com>
2019-05-17 15:25:04 +0900

    feat(rez-pip): add experimental QOL features and pure package indicator
    Add ability to list available versions of requested package compatible
    with current Python interpreter.
    Add ability to query Python version prerequisites for a specific
    version of a package.
    Add ability to constrain package versions compatible with a Python
    version range.
    These are experimental features that use data directly from PyPi
    using the new JSON API.
    Add purity indicator of package upon successful installation.
    
    Relates https://warehouse.readthedocs.io/api-reference/json, https://github.com/nerdvegas/rez/pull/602, https://github.com/nerdvegas/rez/issues/610

--- src/rez/cli/pip.py
+++ src/rez/cli/pip.py
@@ -18,7 +18,16 @@ def setup_parser(parser, completions=False):
         help="install the package")
     parser.add_argument(
         "-s", "--search", action="store_true",
-        help="search for the package on PyPi")
+        help="search for the package on PyPi"),
+    parser.add_argument(
+        "-l", "--list", action="store_true",
+        help="list available versions of a package based on PyPi"),
+    parser.add_argument(
+        "-q", "--query",
+        help="query python pre-requisites of a specific package version on PyPi"),
+    parser.add_argument(
+        "-c", "--constrain",
+        help="find compatible package versions based on Python version constraint before installing"),
     parser.add_argument(
         "-r", "--release", action="store_true",
         help="install as released package; if not set, package is installed "
@@ -30,17 +39,129 @@ def setup_parser(parser, completions=False):
 
 def command(opts, parser, extra_arg_groups=None):
     from rez.pip import pip_install_package, run_pip_command
+    from rez.vendor.version.version import Version, VersionRange
     import sys
+    import json
+    import urllib2
+    import itertools
 
-    if not (opts.search or opts.install):
-        parser.error("Expected one of: --install, --search")
+    if not (opts.search or opts.install or opts.list or opts.query or opts.constrain):
+        parser.error("Expected one of: --install, --search, --list, --query, --constrain")
 
     if opts.search:
         p = run_pip_command(["search", opts.PACKAGE])
         p.wait()
         return
 
-    installed_variants, skipped_variants = pip_install_package(
+    if opts.list:
+        opts.PACKAGE += "=="
+        output = run_pip_command(["install", opts.PACKAGE], process_output=True)
+        for package_version in output:
+            print(package_version)
+        return
+
+    if opts.query:
+        URL = "https://pypi.python.org/pypi/{0}/{1}/json"
+        try:
+            data = json.load(urllib2.urlopen(URL.format(opts.PACKAGE, opts.query)))
+
+            classifiers = data["info"]["classifiers"]
+            python_requires = [classifier for classifier in classifiers if "Programming Language :: Python ::" in classifier]
+            python_requires = ''.join(python_requires)
+            python_requires = python_requires.split("Programming Language :: Python :: ")
+            python_requires = filter(None, python_requires)
+
+            if not python_requires:
+                print("Package version is valid but PyPi does not contain a specific Python version. Too old?")
+                return
+
+            for i, j in itertools.groupby(python_requires, key=lambda x: x[0]):
+                print("Python" + i + ":", [Version(z) for z in list(j) if z != i])
+        except urllib2.HTTPError:
+            print("Package version not found, please check rez-pip -l {0}".format(opts.PACKAGE))
+        return
+
+    option = ""
+    if opts.constrain:
+        ver_range = VersionRange(opts.constrain)
+
+        URL = "https://pypi.python.org/pypi/{0}/json"
+        try:
+            data = json.load(urllib2.urlopen(URL.format(opts.PACKAGE)))
+            releases = data["releases"].keys()
+            releases = sorted(releases)
+
+            python_reqs = {}
+            valid = []
+            suitable = []
+
+            for release in releases:
+                for release_info in data["releases"][release]:
+                    python_version = release_info["python_version"]
+                    requires_python = release_info["requires_python"]
+
+                    if python_version == "source":
+                        continue
+                    elif python_version == "py2.py3":
+                        suitable.append(release)
+                        break
+                    elif python_version == "py3":
+                        if requires_python:
+                            package_range = VersionRange(requires_python)
+                            inrange = ver_range.intersects(package_range)
+                            if inrange:
+                                valid.append(release)
+                                break
+                        else:
+                            python_reqs.setdefault(release, set()).add(Version("3"))
+                    elif python_version.startswith("cp"):
+                        ver = filter(str.isdigit, str(python_version))
+                        raw_ver = ver[:1] + "." + ver[-1]
+                        python_reqs.setdefault(release, set()).add(Version(raw_ver))
+                    else:
+                        python_reqs.setdefault(release, set()).add(Version(python_version))
+
+            for k, v in python_reqs.items():
+                for item in python_reqs[k]:
+                    if ver_range.contains_version(item):
+                        if k not in valid:
+                            valid.append(k)
+                            break
+            print("The following versions of {0} are compatible with Python {1}".format(opts.PACKAGE, opts.constrain))
+            if not valid:
+                print("\tNo package version specifies compatibility with Python version {0}".format(opts.constrain))
+            else:
+                for package_version in sorted(valid):
+                    print("\t" + package_version)
+
+            if suitable:
+                print("")
+                print("")
+
+                print("The following versions of {0} are compatible with all Python versions (unversioned)".format(opts.PACKAGE))
+                for package_version in sorted(suitable):
+                    print("\t" + package_version)
+
+            if valid or suitable:
+                while option != "q" or option not in valid:
+                    option = raw_input("Install version (q to quit): ")
+
+                    if option == "q":
+                        return
+                    elif option in valid:
+                        break
+                    else:
+                        continue
+            else:
+                return
+
+        except urllib2.HTTPError:
+            print("Package version not found, please check rez-pip -l {0}".format(opts.PACKAGE))
+
+    if option:
+        opts.PACKAGE += "=={0}".format(option)
+
+    installed_variants, skipped_variants, variant_types = pip_install_package(
         opts.PACKAGE,
         pip_version=opts.pip_ver,
         python_version=opts.py_ver,
@@ -58,8 +179,9 @@ def command(opts, parser, extra_arg_groups=None):
 
     print()
     if installed_variants:
-        print("%d packages were installed:" % len(installed_variants))
-        for variant in installed_variants:
+        print "%d packages were installed:" % len(installed_variants)
+        for variant, purity in zip(installed_variants, variant_types):
+            print("[{}]".format(purity))
             print_variant(variant)
     else:
         print("NO packages were installed.")
