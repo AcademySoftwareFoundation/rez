f0e6b609b7ba145d63bd7263dafdd166e24d1ad9
marcus <konstruktion@gmail.com>
2019-05-22 12:23:26 +0100

    Increment version Testing this in conjunction with other versions of Rez is a little easier if the version can indicate that it is beta.

--- src/rez/utils/_version.py
+++ src/rez/utils/_version.py
@@ -1,7 +1,7 @@
 
 
 # Update this value to version up Rez. Do not place anything else in this file.
-_rez_version = "2.30.0"
+_rez_version = "2.30.0b1"
 
 try:
     from rez.vendor.version.version import Version
