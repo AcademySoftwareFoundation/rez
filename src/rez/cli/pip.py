2eb9678ff288585bd52336cd8d5d3851884ceb34
lambdaclan <47409392+lambdaclan@users.noreply.github.com>
2019-07-08 11:15:41 +0900

    fix(rez-pip): remove extra line from EOF (2)

--- src/rez/cli/pip.py
+++ src/rez/cli/pip.py
@@ -221,4 +221,3 @@ def command(opts, parser, extra_arg_groups=None):
 #
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library.  If not, see <http://www.gnu.org/licenses/>.
-
--- src/rez/pip.py
+++ src/rez/pip.py
@@ -541,4 +541,3 @@ def _log(msg):
 #
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library.  If not, see <http://www.gnu.org/licenses/>.
-
