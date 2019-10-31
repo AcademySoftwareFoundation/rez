cab6b8ddd06c6c1ebfb3509119f006944f12feed
Will Pittman <willjpittman@gmail.com>
2019-10-31 11:10:02 -0400

    moves test_utils/test_filesystem -> test_utils_filesystem. Adds header/footer so more consistent with other tests.

--- src/rez/tests/test_utils_filesystem.py
+++ src/rez/tests/test_utils_filesystem.py
@@ -1,7 +1,10 @@
-from rez.tests.util import TestBase
-from rez.utils.platform_ import Platform, platform_
-from rez.utils import filesystem
+"""
+unit tests for 'utils.filesystem' module
+"""
 import os
+from rez.tests.util import TestBase
+from rez.utils import filesystem
+from rez.utils.platform_ import Platform, platform_
 
 
 class TestCanonicalPath(TestBase):
@@ -42,3 +45,19 @@ class TestCanonicalPath(TestBase):
         path = filesystem.canonical_path('/var/tmp/File.txt', platform)
         expects = '/var/tmp/file.txt'.replace('\\', os.sep)
         self.assertEqual(path, expects)
+
+
+# Copyright 2013-2016 Allan Johns.
+#
+# This library is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
