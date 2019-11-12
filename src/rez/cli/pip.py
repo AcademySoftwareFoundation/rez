01a9210ff01270d9c3f3af89d2f1aea78e216a83
Sylvain Maziere <sylvain.maziere@mikrosimage.com>
2019-11-12 11:22:52 +0100

    Add 'install_path' argument to rez-pip

--- src/rez/cli/pip.py
+++ src/rez/cli/pip.py
@@ -25,6 +25,9 @@ def setup_parser(parser, completions=False):
         help="install as released package; if not set, package is installed "
         "locally only")
     parser.add_argument(
+         "-p", "--install_path", type=str, metavar='PATH',
+         help="install package to a custom location")
+    parser.add_argument(
         "PACKAGE",
         help="package to install or archive/url to install from")
 
@@ -55,7 +58,8 @@ def command(opts, parser, extra_arg_groups=None):
         opts.PACKAGE,
         pip_version=opts.pip_ver,
         python_version=opts.py_ver,
-        release=opts.release)
+        release=opts.release,
+        install_path=opts.install_path)
 
     # print summary
     #
