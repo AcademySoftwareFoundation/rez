be132f2c5d5ae079c51bd7d4bf036f3c4f4df81e
ajohns <allan.johns@methodstudios.com>
2019-11-12 15:49:34 +1100

    first try at bind mounting the workflow's rez checkout

--- .github/docker/rez-win-py/entrypoint.ps1
+++ .github/docker/rez-win-py/entrypoint.ps1
@@ -1,9 +1,6 @@
 #
 # Entrypoint that installs given python version and runs tests from commit.
 #
-# $repo is the repository like 'nerdvegas/rez'
-# $commit is the revision to test
-param( $repo, $commit )
 
 # Stop on errors; .exe has to be checked manually
 Set-StrictMode -Version latest
@@ -33,16 +30,10 @@ if (-not $?) {exit 1}
 git --version
 if (-not $?) {exit 1}
 
-# Clone repo
-git clone https://github.com/$repo rez
-if (-not $?) {exit 1}
-cd rez
-git checkout $commit
-if (-not $?) {exit 1}
-
 # Install rez
+# Note that the workflow's checkout has been bind mounted to /checkout
 mkdir build
-python install.py build
+python /checkout/install.py build
 if (-not $?) {exit 1}
 
 # Run Rez Tests
