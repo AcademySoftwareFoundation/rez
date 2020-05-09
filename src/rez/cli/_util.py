786f7ece453925cbd53362b922a0fd3728e17534
ajohns <allan.johns@methodstudios.com>
2020-05-09 14:38:36 +1000

    -changed rez-context --res and --req options to print per-line when output is piped -update rez-context --res to print uris if --su is specified

--- src/rez/cli/_util.py
+++ src/rez/cli/_util.py
@@ -127,6 +127,19 @@ def _env_var_true(name):
     return (os.getenv(name, "").lower() in ("1", "true", "on", "yes"))
 
 
+def print_items(items, stream=sys.stdout):
+    try:
+        item_per_line = (not stream.isatty())
+    except:
+        item_per_line = True
+
+    if item_per_line:
+        for item in items:
+            print(item)
+    else:
+        print(' '.join(map(str, items)))
+
+
 def sigbase_handler(signum, frame):
     # show cursor - progress lib may have hidden it
     SHOW_CURSOR = '\x1b[?25h'
