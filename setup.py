60a6c69284fe2004d74a9f997cc6e5b225a93652
ajohns <allan.johns@methodstudios.com>
2021-03-02 13:59:21 +1100

    fix setup.py bug

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ def find_files(pattern, path=None, root="rez"):
 
 
 this_directory = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(os.path.join(this_directory, 'README.md')) as f:
     long_description = f.read()
 
 
