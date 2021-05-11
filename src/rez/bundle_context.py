83f0c07361bb78fa90b2ce58067872e0d79672a9
ajohns <allan.johns@methodstudios.com>
2021-05-11 11:36:32 +1000

    -updated bundle_context to class-based impl

--- src/rez/bundle_context.py
+++ src/rez/bundle_context.py
@@ -33,98 +33,119 @@ def bundle_context(context, dest_dir, force=False, skip_non_relocatable=False,
         quiet (bool): Suppress all output
         verbose (bool): Verbose mode (quiet will override)
     """
-    if quiet:
-        verbose = False
-    if force:
-        skip_non_relocatable = False
-
-    if os.path.exists(dest_dir):
-        raise ContextBundleError("Dest dir must not exist: %s" % dest_dir)
-
-    if not quiet:
-        label = context.load_path or "context"
-        print_info("Bundling %s into %s...", label, dest_dir)
-
-    os.mkdir(dest_dir)
-
-    _init_bundle(dest_dir)
-
-    relocated_package_names = _copy_variants(
+    bundler = _ContextBundler(
         context=context,
-        bundle_dir=dest_dir,
+        dest_dir=dest_dir,
         force=force,
         skip_non_relocatable=skip_non_relocatable,
         verbose=verbose
     )
 
-    rxt_filepath = _write_retargeted_context(
-        context=context,
-        bundle_dir=dest_dir,
-        relocated_package_names=relocated_package_names
-    )
-
-    if verbose:
-        print_info("Context bundled to %s", rxt_filepath)
+    bundler.bundle()
 
 
-def _init_bundle(bundle_dir):
-    # Create bundle conf file. It doesn't contain anything at time of writing,
-    # but its presence on disk signifies that this is a context bundle.
-    #
-    bundle_filepath = os.path.join(bundle_dir, "bundle.yaml")
-    save_yaml(bundle_filepath)
+class _ContextBundler(object):
+    """Performs context bundling.
+    """
+    def __init__(self, context, dest_dir, force=False, skip_non_relocatable=False,
+                 quiet=False, verbose=False):
+        if quiet:
+            verbose = False
+        if force:
+            skip_non_relocatable = False
 
-    # init pkg repo
-    repo_path = os.path.join(bundle_dir, "packages")
-    os.mkdir(repo_path)
+        self.context = context
+        self.dest_dir = dest_dir
+        self.force = force
+        self.skip_non_relocatable = skip_non_relocatable
+        self.quiet = quiet
+        self.verbose = verbose
 
-    # Bundled repos are always memcached disabled because they're on local disk
-    # (so access should be fast); but also, local repo paths written to shared
-    # memcached instance could easily clash.
-    #
-    settings_filepath = os.path.join(bundle_dir, "packages", "settings.yaml")
-    save_yaml(settings_filepath, disable_memcached=True)
+        self.logs = []
 
+    def bundle(self):
+        if os.path.exists(self.dest_dir):
+            raise ContextBundleError("Dest dir must not exist: %s" % self.dest_dir)
 
-def _copy_variants(context, bundle_dir, force=False, skip_non_relocatable=False,
-                   verbose=False):
-    relocated_package_names = []
-    repo_path = os.path.join(bundle_dir, "packages")
+        if not self.quiet:
+            label = self.context.load_path or "context"
+            print_info("Bundling %s into %s...", label, self.dest_dir)
 
-    for variant in context.resolved_packages:
-        package = variant.parent
+        self._init_bundle()
+        relocated_package_names = self._copy_variants()
+        self._write_retargeted_context(relocated_package_names)
+        self._finalize_bundle()
 
-        if skip_non_relocatable and not package.is_relocatable:
-            if verbose:
-                print_warning(
+    @property
+    def _repo_path(self):
+        return os.path.join(self.dest_dir, "packages")
+
+    def _info(self, msg, *nargs):
+        self.logs.append("INFO: %s" % (msg % nargs))
+
+    def _warning(self, msg, *nargs):
+        self.logs.append("WARNING: %s" % (msg % nargs))
+        print_warning(msg, *nargs)
+
+    def _init_bundle(self):
+        os.mkdir(self.dest_dir)
+        os.mkdir(self._repo_path)
+
+        # Bundled repos are always memcached disabled because they're on local disk
+        # (so access should be fast); but also, local repo paths written to shared
+        # memcached instance could easily clash.
+        #
+        settings_filepath = os.path.join(self._repo_path, "settings.yaml")
+        save_yaml(settings_filepath, disable_memcached=True)
+
+    def _finalize_bundle(self):
+        bundle_metafile = os.path.join(self.dest_dir, "bundle.yaml")
+        save_yaml(bundle_metafile, logs=self.logs)
+
+    def _copy_variants(self):
+        relocated_package_names = []
+
+        for variant in self.context.resolved_packages:
+            package = variant.parent
+
+            if self.skip_non_relocatable and not package.is_relocatable:
+                self._warning(
                     "Skipped bundling of non-relocatable package %s",
                     package.qualified_name
                 )
-            continue
+                continue
 
-        copy_package(
-            package=package,
-            dest_repository=repo_path,
-            variants=[variant.index],
-            force=force,
-            keep_timestamp=True,
-            verbose=verbose
+            result = copy_package(
+                package=package,
+                dest_repository=self._repo_path,
+                variants=[variant.index],
+                force=self.force,
+                keep_timestamp=True,
+                verbose=self.verbose
+            )
+
+            assert "copied" in result
+            assert len(result["copied"]) == 1
+            self._info(
+                "Copied %s into %s",
+                result["copied"][0][0].uri,
+                result["copied"][0][1].uri
+            )
+
+            relocated_package_names.append(package.name)
+
+        return relocated_package_names
+
+    def _write_retargeted_context(self, relocated_package_names):
+        rxt_filepath = os.path.join(self.dest_dir, "context.rxt")
+
+        bundled_context = self.context.retargeted(
+            package_paths=[self._repo_path],
+            package_names=relocated_package_names,
+            skip_missing=True
         )
 
-        relocated_package_names.append(package.name)
+        bundled_context.save(rxt_filepath)
 
-    return relocated_package_names
-
-
-def _write_retargeted_context(context, bundle_dir, relocated_package_names):
-    repo_path = os.path.join(bundle_dir, "packages")
-    rxt_filepath = os.path.join(bundle_dir, "context.rxt")
-
-    bundled_context = context.retargeted(
-        package_paths=[repo_path],
-        package_names=relocated_package_names,
-        skip_missing=True
-    )
-
-    bundled_context.save(rxt_filepath)
-    return rxt_filepath
+        if self.verbose:
+            print_info("Bundled context written to to %s", rxt_filepath)
