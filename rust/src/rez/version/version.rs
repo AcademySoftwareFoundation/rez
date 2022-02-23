f5ae1c968ddeb4280a95a4002e37287409d9fd09
Scott Wilson <scott@propersquid.com>
2022-02-22 20:10:18 -0800

    Implement AlphanumericVersionToken

--- rust/src/rez/version/version.rs
+++ rust/src/rez/version/version.rs
@@ -101,7 +101,7 @@ impl NumericToken {
     }
 }
 
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
 struct SubToken {
     s: String,
     n: Option<u64>,
@@ -136,89 +136,6 @@ impl SubToken {
     }
 }
 
-// class AlphanumericVersionToken(VersionToken):
-//     """Alphanumeric version token.
-
-//     These tokens compare as follows:
-//     - each token is split into alpha and numeric groups (subtokens);
-//     - the resulting subtoken list is compared.
-//     - alpha comparison is case-sensitive, numeric comparison is padding-sensitive.
-
-//     Subtokens compare as follows:
-//     - alphas come before numbers;
-//     - alphas are compared alphabetically (_, then A-Z, then a-z);
-//     - numbers are compared numerically. If numbers are equivalent but zero-
-//       padded differently, they are then compared alphabetically. Thus "01" < "1".
-
-//     Some example comparisons that equate to true:
-//     - "3" < "4"
-//     - "01" < "1"
-//     - "beta" < "1"
-//     - "alpha3" < "alpha4"
-//     - "alpha" < "alpha3"
-//     - "gamma33" < "33gamma"
-//     """
-//     numeric_regex = re.compile("[0-9]+")
-//     regex = re.compile(r"[a-zA-Z0-9_]+\Z")
-
-//     def __init__(self, token):
-//         if token is None:
-//             self.subtokens = None
-//         elif not self.regex.match(token):
-//             raise VersionError("Invalid version token: '%s'" % token)
-//         else:
-//             self.subtokens = self._parse(token)
-
-//     @classmethod
-//     def create_random_token_string(cls):
-//         import random
-//         chars = string.digits + string.ascii_letters
-//         return ''.join([chars[random.randint(0, len(chars) - 1)]
-//                        for _ in range(8)])
-
-//     def __str__(self):
-//         return ''.join(map(str, self.subtokens))
-
-//     def __eq__(self, other):
-//         return (self.subtokens == other.subtokens)
-
-//     def less_than(self, other):
-//         return (self.subtokens < other.subtokens)
-
-//     def __next__(self):
-//         other = AlphanumericVersionToken(None)
-//         other.subtokens = self.subtokens[:]
-//         subtok = other.subtokens[-1]
-//         if subtok.n is None:
-//             other.subtokens[-1] = _SubToken(subtok.s + '_')
-//         else:
-//             other.subtokens.append(_SubToken('_'))
-//         return other
-
-//     def next(self):
-//         return self.__next__()
-
-//     @classmethod
-//     def _parse(cls, s):
-//         subtokens = []
-//         alphas = cls.numeric_regex.split(s)
-//         numerics = cls.numeric_regex.findall(s)
-//         b = True
-
-//         while alphas or numerics:
-//             if b:
-//                 alpha = alphas[0]
-//                 alphas = alphas[1:]
-//                 if alpha:
-//                     subtokens.append(_SubToken(alpha))
-//             else:
-//                 numeric = numerics[0]
-//                 numerics = numerics[1:]
-//                 subtokens.append(_SubToken(numeric))
-//             b = not b
-
-//         return subtokens
-
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 struct AlphanumericVersionToken {
     subtokens: Option<Vec<SubToken>>,
@@ -232,7 +149,14 @@ impl VersionToken for AlphanumericVersionToken {}
 
 impl ToString for AlphanumericVersionToken {
     fn to_string(&self) -> String {
-        todo!()
+        match &self.subtokens {
+            Some(subtokens) => subtokens
+                .iter()
+                .map(|subtoken| subtoken.to_string())
+                .collect::<Vec<String>>()
+                .concat(),
+            None => "".to_string(),
+        }
     }
 }
 
@@ -240,7 +164,7 @@ impl FromStr for AlphanumericVersionToken {
     type Err = VersionError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        todo!()
+        Self::new(Some(s))
     }
 }
 
@@ -248,7 +172,29 @@ impl Iterator for AlphanumericVersionToken {
     type Item = AlphanumericVersionToken;
 
     fn next(&mut self) -> Option<Self::Item> {
-        todo!()
+        let mut other = Self::new(None).unwrap();
+
+        // TODO: Not sure if we should return out of the iterator at this point,
+        // or just fail.
+        let mut subtokens = match &self.subtokens {
+            Some(subtokens) => subtokens.to_vec(),
+            None => return None,
+        };
+
+        match subtokens.iter_mut().last() {
+            Some(subtoken) => {
+                if subtoken.n.is_none() {
+                    *subtoken = SubToken::from_str(&format!("{}_", subtoken.s)).unwrap()
+                } else {
+                    subtokens.push(SubToken::from_str("_").unwrap())
+                }
+            }
+            None => return None,
+        };
+
+        other.subtokens = Some(subtokens);
+
+        Some(other)
     }
 }
 
