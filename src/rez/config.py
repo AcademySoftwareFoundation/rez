79e93a7c10ed64bf2144c5892c9ba2c466399074
amorphousWaste <20346603+amorphousWaste@users.noreply.github.com>
2022-06-01 14:26:54 -0700

    Add `skip_root_normalization` to config schema
    Signed-off-by: amorphousWaste <20346603+amorphousWaste@users.noreply.github.com>

--- src/rez/config.py
+++ src/rez/config.py
@@ -468,6 +468,7 @@ config_schema = Schema({
     "context_tracking_amqp":                        OptionalDict,
     "context_tracking_extra_fields":                OptionalDict,
     "optionvars":                                   OptionalDict,
+    "skip_root_normalization":                      OptionalStrList,
 
     # GUI settings
     "use_pyside":                                   Bool,
