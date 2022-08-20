956ecb3f79a85252c7b73dd910255e942b37ee0b
amorphousWaste <20346603+amorphousWaste@users.noreply.github.com>
2022-08-20 00:51:51 -0700

    Add pathing options to rex
    With these options, complex shell implementation can choose to use differently converted paths for different reasons.
    
    Signed-off-by: amorphousWaste <20346603+amorphousWaste@users.noreply.github.com>

--- src/rez/rex.py
+++ src/rez/rex.py
@@ -801,6 +801,45 @@ class Python(ActionInterpreter):
 
         env['SYSTEMROOT'] = os.environ['SYSTEMROOT']
 
+    def as_path(self, path):
+        """
+        Return the given path as a system path.
+        Used if the path needs to be reformatted to suit a specific case.
+        Args:
+            path (str): File path.
+
+        Returns:
+            (str): Transformed file path.
+        """
+        return path
+
+    def as_shell_path(self, path):
+        """
+        Return the given path as a shell path.
+        Used if the shell requires a different pathing structure.
+
+        Args:
+            path (str): File path.
+
+        Returns:
+            (str): Transformed file path.
+        """
+        return path
+
+    def normalize_path(self, path):
+        """
+        Normalize the path to fit the environment.
+        For example, POSIX paths, Windows path, etc. If no transformation is
+        necessary, just return the path.
+
+        Args:
+            path (str): File path.
+
+        Returns:
+            (str): Normalized file path.
+        """
+        return path
+
 
 #===============================================================================
 # String manipulation
