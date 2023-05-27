f32ec772ce1afdbc7c76cd9b2e5f60b058deab58
Jean-Christophe Morin <jean_christophe_morin@hotmail.com>
2023-05-27 17:07:01 -0400

    Fix copyright for real
    Signed-off-by: Jean-Christophe Morin <jean_christophe_morin@hotmail.com>

--- src/rez/data/tests/python/include/utils_test_7cd3a335.py
+++
@@ -1,5 +0,0 @@
-# SPDX-License-Identifier: Apache-2.0
-# Copyright Contributors to the Rez Project
-
-
-# Empty test file for the py23.load_module_from_file tests
--- src/rez/tests/test_utils_py23.py
+++ src/rez/tests/test_utils_py23.py
@@ -7,21 +7,28 @@ unit tests for 'utils.py23' module
 """
 import os
 import sys
+import tempfile
 
-from rez.tests.util import TestBase
+from rez.tests.util import TestBase, TempdirMixin
 from rez.utils import py23
 
 
 class TestLoadModuleFromFile(TestBase):
     def test_load_module(self):
         """Ensure that the imported module does not show up in sys.modules"""
-        include_path = self.data_path('python', 'include')
         # Random chars are used in the module name to ensure that the module name is unique
         # and the test won't fail because some other module with the same name
         # shows up in sys.modules
         module = 'utils_test_7cd3a335'
+
+        filename = '{0}.py'.format(module)
+        tmpdir = tempfile.mkdtemp(prefix="rez_selftest_")
+
+        with open(os.path.join(tmpdir, filename), 'w') as fd:
+            fd.write('')
+
         py23.load_module_from_file(
             module,
-            os.path.join(include_path, '{}.py'.format(module))
+            os.path.join(tmpdir, filename)
         )
         self.assertEqual(sys.modules.get(module), None, msg='Module was found in sys.modules')
