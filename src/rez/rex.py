89b511942affd03dc4535bba7304692cb4d089f4
javrin <jawabiscuit@users.noreply.github.com>
2023-06-26 10:42:15 -0400

    Remove debugging artifact
    Signed-off-by: javrin <jawabiscuit@users.noreply.github.com>

--- src/rez/rex.py
+++ src/rez/rex.py
@@ -589,7 +589,7 @@ class ActionInterpreter(object):
 
     @classmethod
     def _is_shell_pathed_key(cls, key):
-        shell_name = cls.name() if hasattr(cls, 'name') else ''
+        shell_name = cls.name()
         if shell_name not in config.shell_pathed_env_vars:
             return False
 
