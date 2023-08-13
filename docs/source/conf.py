683e16fcbe3a6bf4648b5da8a278cf334cd835d2
Jean-Christophe Morin <jean_christophe_morin@hotmail.com>
2023-08-13 18:36:46 -0400

    Remove unnecessary exclude_patterns from sphinx config
    Signed-off-by: Jean-Christophe Morin <jean_christophe_morin@hotmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,13 +39,6 @@ extensions = [
 ]
 
 templates_path = ['_templates']
-exclude_patterns = [
-    'api/rez.cli.*',
-    'api/rez.vendor.[!v]*',
-    'api/rez.tests.*'
-]
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
