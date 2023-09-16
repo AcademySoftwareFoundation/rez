c679bc9cc6cee9af1cc032a438eb34a6710c4538
Jean-Christophe Morin <jean_christophe_morin@hotmail.com>
2023-09-16 12:14:39 -0400

    Fix copyrights
    Signed-off-by: Jean-Christophe Morin <jean_christophe_morin@hotmail.com>

--- src/rez/version/__init__.py
+++ src/rez/version/__init__.py
@@ -1,5 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright Contributors to the Rez Project
+
+
 from rez.version._requirement import Requirement, RequirementList, VersionedObject
 from rez.version._util import ParseException, VersionError
 from rez.version._version import (
