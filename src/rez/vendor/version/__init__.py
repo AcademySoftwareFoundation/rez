7aa64b97c3ab2bcb3e0132590b9c3943c789cde9
Jean-Christophe Morin <jean_christophe_morin@hotmail.com>
2023-10-28 10:34:38 -0400

    Re-add rez.vendor.version with redirects and a deprecation warning
    Signed-off-by: Jean-Christophe Morin <jean_christophe_morin@hotmail.com>

---
+++ src/rez/vendor/version/__init__.py
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright Contributors to the Rez Project
+
+
+import warnings
+
+warnings.warn(
+    "module 'rez.vendor.version' is deprecated and will be removed in 3.0.0. Use 'rez.version' instead.",
+    DeprecationWarning,
+    stacklevel=2
+)
---
+++ src/rez/vendor/version/util.py
@@ -0,0 +1,5 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright Contributors to the Rez Project
+
+
+from rez.version._util import *
