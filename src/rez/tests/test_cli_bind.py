d92c562cd9c64a0a6977fb331c2445a68cda6b00
Bryce Gattis <brycegattis@yahoo.com>
2024-04-06 19:42:45 -0500

    Add cli bind test
    Signed-off-by: Bryce Gattis <brycegattis@yahoo.com>

---
+++ src/rez/tests/test_cli_bind.py
@@ -0,0 +1,45 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright Contributors to the Rez Project
+
+
+"""
+test running rez bind commandline tool
+"""
+from rez.tests.util import TestBase, TempdirMixin
+import os.path
+import subprocess
+import unittest
+
+from rez.system import system
+
+
+class TestBindCLI(TestBase, TempdirMixin):
+    @classmethod
+    def setUpClass(cls):
+        TempdirMixin.setUpClass()
+
+        cls.install_root = os.path.join(cls.root, "packages")
+
+        cls.settings = dict(
+            packages_path=[cls.install_root]
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        TempdirMixin.tearDownClass()
+
+    def test_basic(self):
+        """run basic bind test"""
+
+        # skip if cli not available
+        if not system.rez_bin_path:
+            self.skipTest("Not a production install")
+
+        binfile = os.path.join(system.rez_bin_path, 'rez-bind')
+        subprocess.check_output([binfile, "platform"])
+        package_exists = os.path.exists(os.path.join(self.install_root, 'platform'))
+        self.assertTrue(package_exists)
+
+
+if __name__ == '__main__':
+    unittest.main()
