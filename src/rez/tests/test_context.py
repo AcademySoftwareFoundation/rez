42402fa3dc07e679fecab6be7f3d86de94bdee9f
BryceGattis <brycegattis@yahoo.com>
2024-07-14 12:11:38 -0500

    Fix broken test
    Signed-off-by: BryceGattis <brycegattis@yahoo.com>

--- src/rez/tests/test_context.py
+++ src/rez/tests/test_context.py
@@ -283,10 +283,9 @@ class TestContext(TestBase, TempdirMixin):
 
         callback = ResolvedContext.Callback(max_fails=999, time_limit=0, callback=solver_callback, buf=sys.stdout)
         solve_state = SolverState(3, 1, None)
-        # TODO: Can't seem to get this mock to work for some reason.
-        # Mock start time to be Jan 1, 1970.
-        with unittest.mock.patch('rez.resolved_context.ResolvedContext.Callback.start_time',
-                                 new_callable=unittest.mock.PropertyMock, return_value=0):
+        with unittest.mock.patch('rez.resolved_context.ResolvedContext.Callback') as mock_callback:
+            # Mock start time to be Jan 1, 1970.
+            mock_callback.start_time.return_value = 0
             callback_result = callback(solve_state)
             assert callback_result[0] == SolverCallbackReturn.abort
 
