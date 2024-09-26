eb7fce53be3b1350e5b6873e0d462856e285958c
Jose Enriquez <alenrqz21@gmail.com>
2024-09-26 17:27:50 -0400

    Add ability to cache package payloads synchronously (#1679)
    Adds a `package_cache_async` flag which allows users to run caching synchronously (blocking) or asynchronously from the config. Also adds CLI argument `--pkg-cache-mode` to manually override the caching mode.
    
    ---
    
    Signed-off-by: ttrently <41705925+ttrently@users.noreply.github.com>
    Signed-off-by: Ben Andersen <ben@isohedron.com.au>
    Signed-off-by: Jean-Christophe Morin <jean_christophe_morin@hotmail.com>
    Co-authored-by: ttrently <41705925+ttrently@users.noreply.github.com>
    Co-authored-by: Jean-Christophe Morin <jean_christophe_morin@hotmail.com>

--- src/rez/tests/test_package_cache.py
+++ src/rez/tests/test_package_cache.py
@@ -134,24 +134,76 @@ class TestPackageCache(TestBase, TempdirMixin):
 
             # Creating the context will asynchronously add variants to the cache
             # in a separate proc.
-            #
+            # NOTE: pyfoo will not cache, because its repo is set to non-caching (see above)
             c = ResolvedContext([
                 "timestamped-1.2.0",
-                "pyfoo-3.1.0"  # won't cache, see earlier test
+                "pyfoo-3.1.0"
             ])
+
+        # Prove that the resolved context used async mode.
+        self.assertTrue(c.package_cache_async)
 
         variant = c.get_resolved_package("timestamped")
 
         # Retry 50 times with 0.1 sec interval, 5 secs is more than enough for
         # the very small variant to be copied to cache.
-        #
         cached_root = None
+        resolve_not_always_cached = False
         for _ in range(50):
-            time.sleep(0.1)
             cached_root = pkgcache.get_cached_root(variant)
             if cached_root:
                 break
 
+            resolve_not_always_cached = True
+            time.sleep(0.1)
+
+        self.assertNotEqual(cached_root, None,
+                            msg="Packages were expected to be cached, but were not.")
+
+        # Test that the package is not immediately cached, since it is asynchronous
+        # WARNING: This is dangerous since it does open the test to a race condition and
+        #   will fail if the cache happens faster than the resolve.
+        self.assertNotEqual(resolve_not_always_cached, False)
+
+        expected_payload_file = os.path.join(cached_root, "stuff.txt")
+        self.assertTrue(os.path.exists(expected_payload_file))
+
+        # check that refs to root point to cache location in rex code
+        for ref in ("resolve.timestamped.root", "'{resolve.timestamped.root}'"):
+            proc = c.execute_rex_code(
+                code="info(%s)" % ref,
+                stdout=subprocess.PIPE,
+                universal_newlines=True
+            )
+
+            out, _ = proc.communicate()
+            root = out.strip()
+
+            self.assertEqual(
+                root, cached_root,
+                "Reference %r should resolve to %s, but resolves to %s"
+                % (ref, cached_root, root)
+            )
+
+    @install_dependent()
+    def test_caching_on_resolve_synchronous(self):
+        """Test that cache is updated as expected on
+        resolved env using syncrhonous package caching."""
+        pkgcache = self._pkgcache()
+
+        with restore_os_environ():
+            # set config settings into env so rez-pkg-cache proc sees them
+            os.environ.update(self.get_settings_env())
+
+            # Creating the context will synchronously add variants to the cache
+            c = ResolvedContext(
+                ["timestamped-1.2.0", "pyfoo-3.1.0"],
+                package_cache_async=False,
+            )
+
+        variant = c.get_resolved_package("timestamped")
+        # The first time we try to access it will be cached, because the cache is blocking
+        cached_root = pkgcache.get_cached_root(variant)
         self.assertNotEqual(cached_root, None)
 
         expected_payload_file = os.path.join(cached_root, "stuff.txt")
