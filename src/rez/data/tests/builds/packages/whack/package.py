6cdb847267478285b3af912a245e03e3e46c10c8
Nathan Rusch <nrusch@users.noreply.github.com>
2025-05-09 17:58:52 -0700

    PowerShell: Fix assumption that `$LASTEXITCODE` is always defined (#1962)
    Signed-off-by: Nathan Rusch <nathan.rusch@scanlinevfx.com>

--- src/rez/data/tests/builds/packages/whack/package.py
+++ src/rez/data/tests/builds/packages/whack/package.py
@@ -4,4 +4,6 @@ authors = ["joe.bloggs"]
 uuid = "4e9f63cbc4794453b0031f0c5ff50759"
 description = "a deliberately broken package"
 
+private_build_requires = ["python"]
+
 build_command = "python {root}/build.py {install}"
