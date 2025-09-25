0957c2f1e5399e9ffac13f317bd16239313f8513
Stephen Mackenzie <maxnbk@users.noreply.github.com>
2025-09-25 17:03:39 -0400

    feat: update vendored modules to: argcomplete-3.1.2, pydot-2.0.0, pyparsing-3.1.4
    Signed-off-by: Stephen Mackenzie <maxnbk@users.noreply.github.com>

---
+++ src/rez/vendor/pydot/__init__.py
@@ -0,0 +1,8 @@
+"""An interface to GraphViz."""
+
+__author__ = "Ero Carrera"
+__version__ = "2.0.0"
+__license__ = "MIT"
+
+from rez.vendor.pydot.exceptions import *
+from rez.vendor.pydot.core import *
