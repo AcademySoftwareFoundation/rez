82c6cffef4e73db29c4a9987a34a0a832b350832
george.ridal <george.ridal@findesign.com.au>
2026-08-01 20:24:37 +1000

    Updating package make to create packages safely - package_maker would not create .building prefix when build payload - changed to use pre_variant_install from repository to get build_prefix - Variant is installed now after make_base and make_root calls - using get_package_payload_path to generate base path
    Signed-off-by: george.ridal <george.ridal@findesign.com.au>

---
+++ src/rez/tests/test_package_maker.py
@@ -0,0 +1,52 @@
+import os
+
+from rez.tests.util import TestBase, TempdirMixin
+from rez.package_maker import make_package
+
+
+class TestPackages(TestBase, TempdirMixin):
+    @classmethod
+    def setUpClass(cls) -> None:
+        TempdirMixin.setUpClass()
+
+        cls.settings = dict()
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        TempdirMixin.tearDownClass()
+
+    def test_make_package(self):
+
+        def make_root(variant, root):
+            assert os.path.isdir(root)
+            assert variant.resource.repository_type == 'memory'
+
+            with open(os.path.join(root, 'payload.txt'),'w'):
+                pass
+
+        with make_package('test_package1', self.root, make_root=make_root) as pkg:
+            pkg.version = '1.0.0'
+
+        assert os.path.isfile(os.path.join(self.root, 'test_package1', '1.0.0', 'package.py'))
+        assert os.path.isfile(os.path.join(self.root, 'test_package1', '1.0.0', 'payload.txt'))
+
+    def test_make_package_with_variant(self):
+
+        def make_root(variant, root):
+            with open(os.path.join(root, 'payload.txt'),'w'):
+                pass
+
+        with make_package('test_package2', self.root, make_root=make_root) as pkg:
+            pkg.version = '1.0.1'
+            pkg.variants = [['python-3']]
+
+        payload_path = os.path.join(self.root, 'test_package2', '1.0.1', 'python-3', 'payload.txt')
+        assert os.path.isfile(payload_path)
+
+    def test_make_package_build_token(self):
+        def make_base(variant, base):
+            assert os.path.isdir(base)
+            assert os.path.isfile(os.path.join(os.path.dirname(base), '.building2.0.1'))
+
+        with make_package('test_package3', self.root, make_base=make_base) as pkg:
+            pkg.version = '2.0.1'
